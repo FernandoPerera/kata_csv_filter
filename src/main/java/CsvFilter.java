1893cdd6e2f77d007368ddfc362efcba65f90130
Fernando Perera <fernando02perera@gmail.com>
2023-03-21 12:10:40 +0100

    Test(green): given_empty_list_return_empty_list, returned the same empty list.

--- src/main/java/CsvFilter.java
+++ src/main/java/CsvFilter.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class CsvFilter {
 
     public List<String> apply(List<String> lines){
-        throw new Error("Not implemented yet");
+        return lines;
     }
 
 }
