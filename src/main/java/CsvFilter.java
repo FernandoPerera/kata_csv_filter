8298aa640c96988bc91704702b87da50976fa58e
Fernando Perera <fernando02perera@gmail.com>
2023-03-21 12:09:06 +0100

    Test(red): given_empty_list_return_empty_list

--- src/main/java/CsvFilter.java
+++ src/main/java/CsvFilter.java
@@ -1,2 +1,9 @@
+import java.util.List;
+
 public class CsvFilter {
+
+    public List<String> apply(List<String> lines){
+        throw new Error("Not implemented yet");
+    }
+
 }
--- src/test/java/CsvFilterTest.java
+++ src/test/java/CsvFilterTest.java
@@ -1,24 +1,28 @@
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CsvFilterTest {
 
-    private final String HEADER_LINE = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
-    private final List<String> emptyDataFile = new ArrayList<>(List.of(HEADER_LINE));
-    private String emptyField = "";
+    /*
+    *   Use cases:
+    *
+    *       - An empty or null list will produce an empty list of output.
+    *
+    */
 
-    private CsvFilter filter = new CsvFilter();
+    private final String HEADER_LINE = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+
+    private final CsvFilter FILTER = new CsvFilter();
 
     @Test
-    void correct_lines_are_not_filtered(){
+    void given_empty_list_return_empty_list(){
+        List<String> lines = List.of();
+        List<String> result = FILTER.apply(lines);
 
+        assertEquals(lines, result);
     }
-
-
 
 }
