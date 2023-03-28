bed784f0c13352b436f9900c7848e73599fe36c0
Fernando Perera <fernando02perera@gmail.com>
2023-03-28 11:41:30 +0200

    Test(refactor): Implemented a boolean for check the iva,igic, nif,and cif existence in the same line.

--- src/main/java/utils/CsvFilter.java
+++ src/main/java/utils/CsvFilter.java
@@ -13,9 +13,9 @@ public class CsvFilter {
             return lines;
         }
 
-        String HEADER_LINE = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
+        String headerLine = "Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente";
 
-        boolean listContainHeader = !lines.get(0).equals(HEADER_LINE);
+        boolean listContainHeader = !lines.get(0).equals(headerLine);
 
         if ( listContainHeader ){
             throw new ListWithoutHeaderExeption("ERROR: don't exist header in the list");
@@ -27,39 +27,30 @@ public class CsvFilter {
     private List<String> checkFieldsOfInvoice(List<String> lines){
 
         List<String> responseList = new ArrayList<>();
+        responseList.add(lines.get(0));
 
-        for ( int index = 0 ; index < lines.size() ; index++ ) {
+        for ( int index = 1 ; index < lines.size() ; index++ ) {
 
             List<String> invoiceLineContentsList = List.of(lines.get(index).split(","));
 
-            if (index == 0) {
-                responseList.add(lines.get(index));
-            } else {
+            String invoiceNumber = invoiceLineContentsList.get(0);
 
-                String invoiceNumber = invoiceLineContentsList.get(0);
+            boolean inLineExistIgicAndIvaOrCifAndNif =
+                    existIvaAndIgicInSameLine(invoiceLineContentsList) && existCifAndNifInSameLine(invoiceLineContentsList);
 
-                if (!checkLinesWhenHaveSamesInvoiceNumbers(invoiceNumber, lines)){
+            if (!linesContentDuplicatedInvoiceNumbers(invoiceNumber, lines) &&
+                    inLineExistIgicAndIvaOrCifAndNif) {
 
-                    if (checkIvaAndIgicFields(invoiceLineContentsList)){
-
-                        if (checkCifAndNifFields(invoiceLineContentsList)){
-
-                            if (checkNetValue(invoiceLineContentsList) ){
-                                responseList.add(lines.get(index));
-                            }
-                        }
-
-                    }
-
+                if (NetValueIsCorrect(invoiceLineContentsList)){
+                    responseList.add(lines.get(index));
                 }
-
             }
         }
 
         return responseList;
     }
 
-    private boolean checkNetValue(List<String> invoiceLineContentsList){
+    private boolean NetValueIsCorrect(List<String> invoiceLineContentsList){
 
         boolean lineContentIva = !invoiceLineContentsList.get(4).isBlank();
 
@@ -73,7 +64,7 @@ public class CsvFilter {
         return netValue == getNetValue(grossValue, invoiceTax);
     }
 
-    private boolean checkCifAndNifFields(List<String> invoiceLineContentsList){
+    private boolean existCifAndNifInSameLine(List<String> invoiceLineContentsList){
 
         boolean lineContentCif = !invoiceLineContentsList.get(6).isBlank();
         boolean lineContentNif = invoiceLineContentsList.size() == 8;
@@ -83,7 +74,7 @@ public class CsvFilter {
         return  !lineContentCif && lineContentNif || lineContentCif && !lineContentNif;
     }
 
-    private boolean checkIvaAndIgicFields(List<String> invoiceLineContentsList){
+    private boolean existIvaAndIgicInSameLine(List<String> invoiceLineContentsList){
 
         boolean lineContentIva = !invoiceLineContentsList.get(4).isBlank();
         boolean lineContentIgic = !invoiceLineContentsList.get(5).isBlank();
@@ -91,7 +82,7 @@ public class CsvFilter {
         return !lineContentIva && lineContentIgic || lineContentIva && !lineContentIgic;
     }
 
-    private boolean checkLinesWhenHaveSamesInvoiceNumbers(String invoiceNumber, List<String> lines){
+    private boolean linesContentDuplicatedInvoiceNumbers(String invoiceNumber, List<String> lines){
 
         int countValue = 0;
 
@@ -104,6 +95,7 @@ public class CsvFilter {
     }
 
     private int getNetValue(int grossValue, int tax){
+
         int grossMinusTaxes = (grossValue * tax) / 100;
         return (grossValue - grossMinusTaxes);
     }
